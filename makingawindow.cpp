f4bae136a3062bc5fa2b05d6018c11d74bae2389
RbCaViTerminal <robert@robertvail.info>
2024-11-20 19:55:49 -0500

    the uv coords have 2 floats

--- makingawindow.cpp
+++ makingawindow.cpp
@@ -242,7 +242,7 @@ int main()
     //enables vertex attributes
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     //--glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
